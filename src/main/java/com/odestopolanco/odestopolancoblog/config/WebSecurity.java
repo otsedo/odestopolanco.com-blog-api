3e4e489665efc0c160a1d27a4c5c2a05d3459dc4
Odesto Polanco <38387829+otsedo@users.noreply.github.com>
2023-11-25 05:22:53 -0400

    Deny all path different that permitted

--- src/main/java/com/odestopolanco/odestopolancoblog/config/WebSecurity.java
+++ src/main/java/com/odestopolanco/odestopolancoblog/config/WebSecurity.java
@@ -20,13 +20,15 @@ public class WebSecurity {
     @Bean
     SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-        http.authorizeHttpRequests(authz ->
-                authz.requestMatchers(HttpMethod.GET, "/v1/users/").hasAnyAuthority(SCOPE_PROFILE)
-                        .requestMatchers(HttpMethod.GET, "/v1/user/{id}").hasAnyAuthority(SCOPE_PROFILE)
-                        .requestMatchers(HttpMethod.POST, "/v1/user").hasAnyAuthority(SCOPE_PROFILE)
-                        .requestMatchers(HttpMethod.PUT, "/v1/user").hasAnyAuthority(SCOPE_PROFILE)
-                        .anyRequest().authenticated()).oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {
-        }));
+        http.authorizeHttpRequests(authorize ->
+                        authorize
+                                .requestMatchers(HttpMethod.GET, "/v1/users/**").hasAnyAuthority(SCOPE_PROFILE)
+                                .requestMatchers(HttpMethod.POST, "/v1/users/**").hasAnyAuthority(SCOPE_PROFILE)
+                                .requestMatchers(HttpMethod.PUT, "/v1/users/**").hasAnyAuthority(SCOPE_PROFILE)
+                                .requestMatchers(HttpMethod.GET, "/v1/posts/**").hasAnyAuthority(SCOPE_PROFILE)
+                                .anyRequest().denyAll())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {
+                }));
         return http.build();
     }
 }
--- src/main/java/com/odestopolanco/odestopolancoblog/controller/v1/UserController.java
+++ src/main/java/com/odestopolanco/odestopolancoblog/controller/v1/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
